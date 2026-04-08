aa327128731a6548571189b2db97d21c8c25b5d6
Stephane <stephaneagg@gmail.com>
2026-04-08 12:24:11 -0700

    fixed styling and rendered the new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 
-
+import Status from "./Status.jsx"
+import Lives from "./Lives.jsx"
+import Word from "./Word.jsx"
+import { useState } from "react"
 export default function App() {
+
+
+  const [word, setWord] = useState("react")
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: 
+ * Display the keyboard ⌨️. Use <button>s for each letter
+ * since it'll need to be clickable and tab-accessible.
+ */
+
+
   return (
     <main>
-      <p>Hello World!</p>
+      <header>
+        <h1>HangMouse</h1>
+        <p>Guess the word in under 6 attempts to win</p>
+      </header>
+
+
+        <Status />
+        <Lives />
+        <Word word={word}/>
+
     </main>
   )
 }
